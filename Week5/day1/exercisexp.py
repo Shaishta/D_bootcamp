bb52b28963a1c0ec6bd346860801be305b315bbf
Shaishta <91430606+Shaishta@users.noreply.github.com>
2021-11-23 22:36:09 +0400

    Updated and corrected XP

--- Week5/day1/exercisexp.py
+++ Week5/day1/exercisexp.py
@@ -67,46 +67,57 @@
 # print(stairway.sing_me_a_song())
 
 # #Exercise 4 : Afternoon At The Zoo
+
 class Zoo:
     def __init__(self, zoo_name):
-        self.zoo_name = zoo_name
+        self.name = zoo_name
         self.animals = []
-
+        self.pens = {}
     def add_animal(self, new_animal):
         if new_animal not in self.animals:
             self.animals.append(new_animal)
         else:
-            print(f"{new_animal} already exists")
-
+            pass
     def get_animals(self):
-        for animal in self.animals:
-            print(animal)
-
+        print(",".join(self.animals))
     def sell_animal(self, animal_sold):
         if animal_sold in self.animals:
+            print(f"Goodbye {animal_sold} !")
             self.animals.remove(animal_sold)
         else:
-            print(f"{animal_sold} not in the zoo")
+            print(f"{animal_sold} is not in the zoo.")
+    def sort_animal(self):
+        copy = self.animals.copy()
+        copy.sort()
+        list_of_lists = []
+        temp = []
+        temp.append(copy.pop(0))
+        while len(copy) > 0:
+            if temp[0][0] == copy[0][0]:
+                temp.append(copy.pop(0))
+            else:
+                list_of_lists.append(temp)
+                temp = []
+                temp.append(copy.pop(0))
+        list_of_lists.append(temp)
+        num_list = [i for i in range(len(list_of_lists))]
+        result_dict = {key: value for key, value in zip(num_list, list_of_lists)}
+        self.pens = result_dict
+    def get_pens(self):
+        for pen in self.pens.items():
+            print(f"in pen {pen[0]} there are: {','.join(pen[1])}")
 
-    def sort_animals(self):
-        self.animals.sort()
-        self.animals = [list(filter(lambda animal: animal[0] == l, self.animals)) for l in sorted(list(set([animal[0] for animal in self.animals])))]
-
-    def get_groups(self):
-        for animal in self.animals:
-            print(animal)
-
-ramat_gan_safari = Zoo('Ramat Gan Safari')
-
-ramat_gan_safari.add_animal('lion')
-ramat_gan_safari.add_animal('giraffe')
-ramat_gan_safari.add_animal('zebra')
-ramat_gan_safari.add_animal('tiger')
-ramat_gan_safari.add_animal('elephant')
-ramat_gan_safari.add_animal('monkey')
-ramat_gan_safari.add_animal('beer')
-ramat_gan_safari.get_animals()
-ramat_gan_safari.sell_animal('zebra')
-ramat_gan_safari.sort_animals()
-ramat_gan_safari.get_groups()
-ramat_gan_safari.get_animals()
+zoo = Zoo('Casela nature park')
+zoo.add_animal('Lion')
+zoo.add_animal('Giraffe')
+zoo.add_animal('Leopard')
+zoo.add_animal('Bear')
+zoo.add_animal('Bat')
+zoo.get_animals()
+zoo.sell_animal('Lizard')
+zoo.sell_animal('Lion')
+zoo.get_animals()
+zoo.add_animal('Lion')
+# sorted_animals = zoo.sort_animal()
+zoo.sort_animal()
+zoo.get_pens()
