8fadc19152d7a013bb2c177fa364795447460a67
Shaishta <91430606+Shaishta@users.noreply.github.com>
2021-12-05 21:10:32 +0400

    edited menu_editor.py

--- Week6/Day5/menu_editor.py
+++ Week6/Day5/menu_editor.py
@@ -2,62 +2,49 @@
 #Part 2
 from exercisexp import MenuItem
 
-
-def load_manager(name, price):
-    m = MenuItem(name, price)
-    return m
-
+def load_manager():
+    new_menu = MenuItem()
+    return new_menu
 
 def show_user_menu():
-    print('MENU')
-    user_choice = input('''
-    (a) Add an item
-    (d) Delete an Item
-    (v) View the menu
-    (x) Exit
-    :
-    ''')
-    return user_choice
-
-def add_item_to_menu(name ,price):
-    load_manager(name, price).save()
-
-
-def remove_item_from_menu(name, price=None):
-    if load_manager(name, price).get_by_name(name):
-        load_manager(name, price).delete()
-        print('removed item')
+    print('''
+    Menu
+    1. Add an item
+    2. Delete an item
+    3. View the menu
+    4. Exit''')
+    ans = input('Please enter choice from 1 to 4: ')
+    if ans == '1':
+        return add_item_to_menu()
+    elif ans == '2':
+        return remove_item_from_menu()
+    elif ans == '3':
+        return show_restaurant_menu()
+    elif ans == '4':
+        print('See you soon! Goodbye')
+        return
     else:
-        print('error')
+        print("Error. Please try again!")
+        return show_user_menu()
 
+def add_item_to_menu():
+    name = input('Enter the name of the item: ')
+    price = int(input('Enter the price of the item: '))
+    item = MenuItem(name, price).save()
+    print('item was added successfully')
+    return show_user_menu()
 
-def show_restaurant_menu(name=None, price=None):
-    load_manager(name, price).all()
+def remove_item_from_menu():
+    name = input('Enter the name of the item: ')
+    item = MenuItem(name).delete()
+    if MenuItem().get_by_name(item) == None:
+        print('item was deleted')
+    else:
+        print("error")
+    return show_user_menu()
 
+def show_restaurant_menu():
+    print(MenuItem().all())
+    return show_user_menu()
 
-def run():
-    while True:
-        user_choice = show_user_menu()
-        if user_choice == "a" or user_choice == "d" or user_choice == "v" or user_choice == "x":
-            if user_choice == "a":
-                item = input('choose item name:')
-                if not item.isdigit():
-                    item_price = input('choose item price:')
-                    add_item_to_menu(item, item_price)
-                else:
-                    print('invalid type')
-            elif user_choice == "d":
-                item = input('choose item name:')
-                remove_item_from_menu(item)
-            elif user_choice == "v":
-                show_restaurant_menu()
-            else:
-                print('Goodbye!')
-                show_restaurant_menu()
-                break
-        else:
-            print('invalid input')
-
-
-run()
-
+show_user_menu()
